05b4b3d5320f29ab62f672d5f6f1ad09696d5f6a
NicoleRöhm <119949984+NicoleRoehm@users.noreply.github.com>
2023-02-06 16:44:31 +0100

    Delete ContactName.kt

--- ContactName.kt
+++
@@ -1,9 +0,0 @@
-package com.example.writeme.data.model
-
-
-data class ContactName (
-    val id: Int,
-    val name: String,
-    val imageResourceId : Int,
-    val historyChat: MutableList<String>
-        )
